fdd3c0573a06b1ff2b4ca63bc8e54b08bc7d3858
Carlos Jones <carlosjones@Carloss-MBP.home>
2015-06-17 01:52:10 -0400

    First commit

--- public/index.js
+++ public/index.js
@@ -1,1 +1,62 @@
-/* Your JS code goes here! */
+$(document).on("ready", function(){
+
+	//$(".name").on("click", function(){
+     //$(".name").removeClass("active")
+     //$(this).addClass("active")
+     //$(this).highlight("active")
+//})
+
+//$(".name").hover(function() {               
+     // $(this).addClass('highlight');  
+   //}, function() {  
+   //   $(this).removeClass('highlight');  
+ //})
+
+
+
+
+	var templateremedy = Handlebars.compile($("#right-side").html() )
+	
+	$.ajax({
+	  url: "/remedy",
+	  method: "GET",
+	  data: {
+	  },
+	  success: function(remedies) {
+	  	var htmlString = templateremedy(remedies)
+	  	$("#right").append( htmlString )
+	  }
+      
+	})
+
+    var templateleft = Handlebars.compile( $("#left-side").html() )
+   
+   	$.ajax({
+	  url: "/remedy",
+	  method: "GET",
+	  data: {
+
+	  },
+	  success: function(data) { 
+	  	_.each(data.remedies, function(data){
+	  		var htmlString = templateleft(data)
+		  	$("#left").append( htmlString )
+	  	})
+	  	
+	  } 
+    
+    })
+
+$(".hide").hide();
+$(".name.active").show();
+
+$(".name").click(function(){
+$(".hide").slideToggle();
+ }) 
+
+})
+
+
+
+
+
