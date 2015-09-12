e02ed6688ce4dba2900ce5c63a76cd660e166c1b
Carlos Jones <carlosjones@Carloss-MBP.home>
2015-09-12 17:26:59 -0400

    Final

--- public/index.js
+++ public/index.js
@@ -1,32 +1,71 @@
 
-
-$(document).on("ready", function() {
-
-	var coldRemedyFunction = Handlebars.compile( $("#coldRemediesTemplate").html() )
-
+var remedyList = function(callback){
+	
 	$.ajax({
-
 		url: "/remedy",
-		method: "GET",
-		data: {},
-		success: function(data) {
-
-			_.each(data, function(id) {
-
-				_.each(id, function(remedy) {
-					var htmlString = coldRemedyFunction(remedy)
-
-					console.log(htmlString)
-
-					$(".listOfRemedies").append(htmlString)
-				})
-				
-
-			})
-
-				
+		method:"GET",
+		success: function(data){
+		callback(data)
 		}
 
 	})
 
-})
+}
+
+ var strength = function(i){
+ 	if (i === 3){
+ 		return "Probably effective"
+ 	}
+ 	
+ 	if (i === 2){
+ 		return "Possibly effective"
+ 	}
+
+ 	if (i === 1){
+		return "Unlikely to be effective"
+	}
+
+	if (i === 0){
+		return "Ineffective"
+	}
+
+ }
+
+
+var remedyStuff= function(data){
+
+	_.each(data.remedies, function(remedy){
+
+		var newRemedy = {
+			name: remedy.name,
+			prevention: strength(remedy.prevention),
+			treatment: strength(remedy.treatment),
+			description: remedy.description,
+			reaction: remedy.reaction
+		}
+
+
+		var htmlLeft = templateLeft(newRemedy)
+			$("#cold-rem").append(htmlLeft)
+		})	
+
+		$(".remedy").on("click", function(){
+			$(this).find(".description").toggleClass("active")
+
+			console.log(this)
+		})
+
+
+
+
+}
+
+
+	$(document).on("ready", function(){
+		templateLeft = Handlebars.compile( $("#coldRemediesTemplate").html() );
+
+
+		remedyList(remedyStuff)
+
+
+	})
